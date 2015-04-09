478729f82e247f2af502ef7203fe59dec471e8e2
Florian Landerl <florian@platogo.com>
2015-04-09 15:57:13 +0200

    Fix parameter name

--- www/android-unzip.js
+++ www/android-unzip.js
@@ -1,5 +1,5 @@
 module.exports = {
-    unzip: function(zipPath, destPath, successCallback, errorCallback, progressCallback) {
+    unzip: function(zipFilePath, destPath, successCallback, errorCallback, progressCallback) {
         var successProxy = function(result) {
             if (result && typeof result.loaded != "undefined") {
                 if (progressCallback) {
